7342fb3bb2d1bf44aded945578b305489ac3e7a4
Soha Rahman <studentinsha12@gmail.com>
2017-11-06 06:53:50 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,6 @@
 function forLoop(array) {
-  for(var i=0;i<25;i++) {
-    i==1?array.push('I am  1 strange loop'):array.push(`"I am ${i} strange loop${i===0?'':'s'}"`);
+  for(i=0;i<25;i++) {
+    array.push(`I am ${i} strange loop${(i===1||i===0)?'';'s'}`);
   }
+  return array;
 }
